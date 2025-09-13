603b7f026a0f76dbb06b8b2393e13e073cc9c683
abdurahman <abdurrahmanzerihun@gmail.com>
2025-09-13 14:08:31 +0300

    Add getComputerChoice()
    To get random computer choice using
    getComputerChoice() and
    inside it Math.random()

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let string="Hello world";
-console.log(string);
+//get random computer choice using 
+function getComputerChoice(){
+const max=3;
+const randomNumber=Math.floor(Math.random()*max)
+if(randomNumber===0){
+       
+        console.log("Rock")
+}
+else if(randomNumber===1){
+        
+        console.log("Paper")
+}
+  else if(randomNumber===2) {
+   console.log("Scissor")     
+  }   
+
+        
+
+}
+getComputerChoice();
