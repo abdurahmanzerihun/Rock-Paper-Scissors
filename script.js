9334b19399c2c7e425fd7d8106a7c56ba1158d58
abdurahman <abdurrahmanzerihun@gmail.com>
2025-09-14 06:39:26 +0300

    Add getHumanChoice()

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ else if(randomNumber===1){
 
 }
 getComputerChoice();
+
+//get human choice
+function getHumanChoice(){
+console.log("Enter a valid data between 1-3"); 
+ console.log( "1.Rock")    ;
+console.log("2. Paper")   ;
+ console.log("3.Scissor") ;
+ let choice=parseInt(prompt("Enter your choice:",''));
+ if(choice===1){
+        console.log("Rock")
+ }
+ else if(choice===2){
+        console.log("Paper")
+ }
+ else if(choice===3){
+        console.log("Scisssor")
+ }
+else {
+        console.log("Invalid data")
+}
+}
+getHumanChoice();
