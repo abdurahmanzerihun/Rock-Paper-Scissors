27580dbc7dffbd6d24e8247ae4f60934b54cee2c
abdurahman <abdurrahmanzerihun@gmail.com>
2025-10-01 12:23:29 +0300

    Arrange the indentation

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let round=0;
-let maxRound=5;
-let humanSelections=document.querySelectorAll('.choices button');
-let gameResult=document.getElementById('gameResult');
-let humanScore=0;
-let computerScore=0;
-let choiceResult=document.getElementById('choiceResult');
-let humanScoreV=document.getElementById('humanScore');
-let computerScoreV=document.getElementById('computerScore');
-let reset=document.getElementById('reset');
-let anounce=document.getElementById('anounce');
+let round = 0;
+let maxRound = 5;
+let humanSelections = document.querySelectorAll('.choices button');
+let gameResult = document.getElementById('gameResult');
+let humanScore = 0;
+let computerScore = 0;
+let choiceResult = document.getElementById('choiceResult');
+let humanScoreV = document.getElementById('humanScore');
+let computerScoreV = document.getElementById('computerScore');
+let reset = document.getElementById('reset');
+let anounce = document.getElementById('anounce');
 
 humanSelections.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -16,90 +16,87 @@ humanSelections.forEach(button => {
         playRound(humanSelection);
     });
 });
-function getComputerChoice(){
- //get random computer choice 
-const max=3;
-const randomNumber=Math.floor(Math.random()*max)
-if(randomNumber===0){
-      return "Rock";
-       }
-else if(randomNumber===1){
+
+function getComputerChoice() {
+    // get random computer choice 
+    const max = 3;
+    const randomNumber = Math.floor(Math.random() * max);
+    if (randomNumber === 0) {
+        return "Rock";
+    }
+    else if (randomNumber === 1) {
         return "Paper";
-        }
-  else if(randomNumber===2) {
+    }
+    else if (randomNumber === 2) {
         return "Scissor";
-        }   
+    }
 }
 
-function playRound(humanSelection){
-        if(round>=maxRound){
-                return;
-        }
-        const computerSelection=getComputerChoice();
-if(humanSelection===computerSelection)
-{
-       choiceResult.textContent= `Your choice: ${humanSelection} | Computer Choice:${computerSelection}`
-       gameResult.textContent= `Round:${round+1} | Draw`;
+function playRound(humanSelection) {
+    if (round >= maxRound) {
+        return;
+    }
+    const computerSelection = getComputerChoice();
+
+    if (humanSelection === computerSelection) {
+        choiceResult.textContent = `Your choice: ${humanSelection} | Computer Choice: ${computerSelection}`;
+        gameResult.textContent = `Round: ${round + 1} | Draw`;
         humanScore++;
         computerScore++;
-        
-}
-else if((humanSelection==="Rock")&&(computerSelection==="Scissor")||
-(humanSelection==="Scissor")&&(computerSelection==="Paper") ||
-(humanSelection==="Paper")&&(computerSelection==="Rock"))
-{
-       choiceResult.textContent=`Your choice: ${humanSelection} | Computer Choice:${computerSelection}`
-       gameResult.textContent=`Round:${round+1}| You win! ${humanSelection} beats ${computerSelection}`;
+    }
+    else if (
+        (humanSelection === "Rock" && computerSelection === "Scissor") ||
+        (humanSelection === "Scissor" && computerSelection === "Paper") ||
+        (humanSelection === "Paper" && computerSelection === "Rock")
+    ) {
+        choiceResult.textContent = `Your choice: ${humanSelection} | Computer Choice: ${computerSelection}`;
+        gameResult.textContent = `Round: ${round + 1} | You win! ${humanSelection} beats ${computerSelection}`;
         humanScore++;
-}
-  
-else if ((computerSelection==="Rock")&&(humanSelection==="Scissor")||
-(computerSelection==="Scissor")&&(humanSelection==="Paper") ||
-(computerSelection==="Paper")&&(humanSelection==="Rock"))
-{
-        choiceResult.textContent=`Your choice: ${humanSelection} | Computer Choice:${computerSelection}`;
-        gameResult.textContent=`Round:${round+1} | You lose! ${computerSelection} beats ${humanSelection}`;
+    }
+    else if (
+        (computerSelection === "Rock" && humanSelection === "Scissor") ||
+        (computerSelection === "Scissor" && humanSelection === "Paper") ||
+        (computerSelection === "Paper" && humanSelection === "Rock")
+    ) {
+        choiceResult.textContent = `Your choice: ${humanSelection} | Computer Choice: ${computerSelection}`;
+        gameResult.textContent = `Round: ${round + 1} | You lose! ${computerSelection} beats ${humanSelection}`;
         computerScore++;
-}
-else
-{
-         gameResult.textContent="Invalid Choice";
-}
-      round++;
-        humanScoreV.textContent=`Your Score:${humanScore}`;
-        computerScoreV.textContent=`Computer Score:${computerScore}`;
-        if(round===maxRound){
-                anounceWinner();
-        }
+    }
+    else {
+        gameResult.textContent = "Invalid Choice";
+    }
 
-}
-function anounceWinner(){
-  
-  if(humanScore>computerScore)
-        {
-        anounce.textContent="Congratulation! You won the game!"; 
-        anounce.style.color="green";
-         }
- else if(computerScore>humanScore)
-        {
-        anounce.textContent="Sorry! You failed the game!";
-         anounce.style.color="red";
-        } 
-        else
-        {
-        anounce.textContent="It's a tie";
-         anounce.style.color="rgb(119, 119, 119) ";
-        }
-}
-reset.addEventListener('click',(event)=>{
-        humanScore=0;
-        computerScore=0;
-        round=0;
-        anounce.textContent="";
-         humanScoreV.textContent=`Your Score:${humanScore}`;
-        computerScoreV.textContent=`Computer Score:${computerScore}`;
-         gameResult.textContent=`Round:${round}`;
-         choiceResult.textContent=`Choose your weapon !`;
+    round++;
+    humanScoreV.textContent = `Your Score:${humanScore}`;
+    computerScoreV.textContent = `Computer Score:${computerScore}`;
 
+    if (round === maxRound) {
+        anounceWinner();
+    }
+}
+
+function anounceWinner() {
+    if (humanScore > computerScore) {
+        anounce.textContent = "Congratulation! You won the game!";
+        anounce.style.color = "green";
+    }
+    else if (computerScore > humanScore) {
+        anounce.textContent = "Sorry! You failed the game!";
+        anounce.style.color = "red";
+    }
+    else {
+        anounce.textContent = "It's a tie";
+        anounce.style.color = "rgb(119, 119, 119)";
+    }
+}
+
+reset.addEventListener('click', (event) => {
+    humanScore = 0;
+    computerScore = 0;
+    round = 0;
+    anounce.textContent = "";
+    humanScoreV.textContent = `Your Score:${humanScore}`;
+    computerScoreV.textContent = `Computer Score:${computerScore}`;
+    gameResult.textContent = `Round:${round}`;
+    choiceResult.textContent = `Choose your weapon !`;
 });
-
